22c6666293e51d5b1a36e6968afe65fb274c4859
Carl Ji <jixy2018@gmail.com>
2026-02-28 19:17:50 -0800

    fix: remove unnecessary scripts

--- src/openwrc/services/base_service.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from openwrc.clients.wrc_api_client import WrcApiClient
-
-
-class BaseService:
-    def __init__(self, external_api_client: Optional[WrcApiClient] = None) -> None:
-        self.external_api_client = external_api_client or WrcApiClient()
